afe75549027ce5c545fa689e38d203a3fff61f61
Vikki6408 <vikki.dolt@eduvaud.ch>
2025-11-25 13:14:53 +0100

    feat: validatePrice

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -71,6 +71,9 @@ module.exports = class CartItem {
 
     #validatePrice(price) {
         //TODO Implement this method
+        if (price < 10) {
+            throw new InvalidPriceException();
+        }
     }
     //endregion private methods
 }
